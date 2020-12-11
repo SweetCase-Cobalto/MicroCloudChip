595034415047776f9d2355bae75d808078024153
ReicomA <seokbong60@gmail.com>
2020-12-11 14:48:41 +0900

    make login page

--- app/static/app/js/login.js
+++ app/static/app/js/login.js
@@ -4,4 +4,10 @@ $(() => {
         
         document.getElementById('editor').submit();
     });
+    $('#login-btn').hover(() => {
+        $('#login-btn').css("color", "#AECCD1");
+        console.log('a');
+    }, () => {
+        $('#login-btn').css("color", "#277BBA");
+    });
 });
